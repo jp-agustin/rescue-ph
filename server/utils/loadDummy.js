92887952c0af98c6de34f6e070813a2717b6da26
jp-agustin <agustin.johnpatrick@gmail.com>
2020-11-24 21:33:24 +0800

    Add dummy rescue updates data

--- server/utils/loadDummy.js
+++ server/utils/loadDummy.js
@@ -2,21 +2,48 @@ const fs = require('fs');
 const path = require('path');
 const bunyan = require('bunyan');
 
+const isEmpty = require(path.join(__dirname, './isEmpty'));
+
 const log = bunyan.createLogger({ name: 'load-dummy' });
 
-const { Rescue } = require(path.join(__dirname, '../models/'));
+const { Rescue, Update } = require(path.join(__dirname, '../models/'));
+
+const addUpdates = (updates, rescue, ind) => {
+  const breakpoints = [[0, 1], [], [2, 3], [4, 4], [5, 6], [7, 7], [], [8, 8], [9, 9], [10, 11]];
+
+  if (!isEmpty(breakpoints[ind])) {
+    for (let i = breakpoints[ind][0]; i <= breakpoints[ind][1]; i += 1) {
+      const newUpdateEntry = new Update({
+        rescueId: rescue,
+        timestamp: new Date(updates[i].timestamp),
+        update: updates[i].update,
+      });
+
+      newUpdateEntry
+        .save()
+        .then(() => log.info('Update successfully added'))
+        .catch((err) => {
+          log.error(err);
+        });
+    }
+  }
+
+  log.info('Rescue successfully added');
+};
 
 const loadDummy = () => {
-  const rawData = fs.readFileSync(path.join(__dirname, './dummy/', 'rescues.json'));
-  const rescues = JSON.parse(rawData);
+  const rawRescuesData = fs.readFileSync(path.join(__dirname, './dummy/', 'rescues.json'));
+  const rawUpdatesData = fs.readFileSync(path.join(__dirname, './dummy/', 'updates.json'));
+  const rescues = JSON.parse(rawRescuesData);
+  const updates = JSON.parse(rawUpdatesData);
 
-  rescues.forEach((rescue) => {
-    const newEntry = new Rescue(rescue);
+  rescues.forEach((rescue, ind) => {
+    const newRescueEntry = new Rescue(rescue);
 
-    newEntry
+    newRescueEntry
       .save()
-      .then(() => {
-        log.info('Entry successfully added');
+      .then((savedRescue) => {
+        addUpdates(updates, savedRescue, ind);
       })
       .catch((err) => {
         log.error(err);
