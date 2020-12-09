406db2ed9f1c8d4ed89fd8aaebac3a269c9ceff0
jacintojosh <jacintojosh@gmail.com>
2020-12-10 02:33:40 +0800

    Set query param as string.

--- server/services/graph-api.js
+++ server/services/graph-api.js
@@ -5,17 +5,13 @@ const log = bunyan.createLogger({ name: 'graph-api-service' });
 
 module.exports = class GraphAPi {
   static callSendAPI(requestBody) {
-    axios
-      .post('https://graph.facebook.com/v3.2/me/messages', {
-        query: { access_token: process.env.PAGE_ACCESS_TOKEN },
-        data: { requestBody },
-      })
-      .catch((error) => {
-        if (error) {
-          log.error('Unable to send message:', error);
-        } else {
-          log.info('message sent!');
-        }
-      });
+    const messengerUrl = `https://graph.facebook.com/v3.2/me/messages?access_token=${process.env.PAGE_ACCESS_TOKEN}`;
+    axios.post(messengerUrl, { requestBody }).catch((error) => {
+      if (error) {
+        log.error('Unable to send message:', error);
+      } else {
+        log.info('message sent!');
+      }
+    });
   }
 };
