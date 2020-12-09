b2534fc71587e420632d6d6d0b1802a70849d2d0
jacintojosh <jacintojosh@gmail.com>
2020-12-10 02:36:35 +0800

    Revert previous commit, and pass requestBody as is.

--- server/services/graph-api.js
+++ server/services/graph-api.js
@@ -5,13 +5,16 @@ const log = bunyan.createLogger({ name: 'graph-api-service' });
 
 module.exports = class GraphAPi {
   static callSendAPI(requestBody) {
-    const messengerUrl = `https://graph.facebook.com/v3.2/me/messages?access_token=${process.env.PAGE_ACCESS_TOKEN}`;
-    axios.post(messengerUrl, { requestBody }).catch((error) => {
-      if (error) {
-        log.error('Unable to send message:', error);
-      } else {
-        log.info('message sent!');
-      }
-    });
+    axios
+      .post('https://graph.facebook.com/v3.2/me/messages', requestBody, {
+        params: { access_token: process.env.PAGE_ACCESS_TOKEN },
+      })
+      .catch((error) => {
+        if (error) {
+          log.error('Unable to send message:', error);
+        } else {
+          log.info('message sent!');
+        }
+      });
   }
 };
